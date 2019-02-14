1eced7089f23a1e249e5735a0bec222ce56e86ab
Alex <AlexGreen2015@icloud.com>
2019-02-14 04:11:49 -0600

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,7 +26,7 @@ loadMainPage = function(){
             <img class="img-fluid"  src="assets/images/alex.jpg">
         </div>
         <div class='col-8'>
-        <p class="lead">Who I am: Ambitious self-taught FullStack web developer with very high learning ability. Flexible and adaptive to new environments with a high desire to achieve. I’m extremely passionate about web development and design in all it’s forms and helping small businesses and artists build and improve their online presence.</p>
+        <p class="lead">Full Stack Web Developer with a background in applied math and programming. Strong engineering professional skills in both Front-end and Back-end Development with outstanding knowledge of  HTML CSS and Javascript. Flexible and adaptive to new environments with a high desire to achieve. I enjoy coding, debugging and solving problems. I’m extremely passionate about web development and design in all its forms helping small businesses and artists build and improve their online presence. Always open to learn new and share my knowledge with others. I hold communication in high regard and work well when collaborating with others or independently. Recently I've graduated from Northwestern Coding Bootcamp witch helped me to develop many of the skills required to be a great Web Developer.</p>
         <p class="lead">Education: Belarusian State Univercity 2009-2012 (Applied math and programming)</p>
         <p class="lead">Online courses : CodeCademy, Coursera , Khan Academy...</p>
         <p class="lead">Now: Northwestern Coding Bootcamp.</p>
@@ -38,15 +38,14 @@ loadMainPage = function(){
         <h3 id="core">Core Skills:</h3>
         <ul id="qua">
         <li class="lead">Outstanding knowledge of web programming languages including HTML CSS JavaScript</li>
-        <li class="lead">Superior facility with Windows Linux UNIX and MAC operating systems</li>
         <li class="lead">Bootstrap / AngularJS / Responsive Web Design</li>
-        <li class="lead">Experiense with node.js / npm / git / terminal</li>
+        <li class="lead">node.js / npm / git / terminal</li>
         <li class="lead">Google Firebase DB development</li>
-        <li class="lead">MongoDb / REST API / MEAN stack development</li>
-        <li class="lead">CMS - WordPress</li>
+        <li class="lead">MongoDb / REST API / MERN stack development</li>
+        <li class="lead">CMS</li>
         <li class="lead">Profound creative and analytical problem-solving and troubleshooting skills</li>
         <li class="lead">Exceptional organization and presentation abilities</li>
-        <li class="lead">High verbal and written communication skills</li>
+        <li class="lead">Superior facility with Windows Linux UNIX and MAC operating systems</li>
         </ul>
         </div>
     </div>
